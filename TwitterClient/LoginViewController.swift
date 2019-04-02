0d027bf1799452e97ec8f6d4b4bacc999b29ec27
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-04-01 20:03:54 -0700

    Layout initial UI:
    - Create LoginViewController with login button
    - Add a navigation controller (to be presented modally on login)
    - Create action for login button

---
+++ TwitterClient/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  TwitterClient
+//
+//  Created by Maribel Montejano on 4/1/19.
+//  Copyright © 2019 Maribel Montejano. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func onLoginButton(_ sender: UIButton) {
+        
+    }
+}
