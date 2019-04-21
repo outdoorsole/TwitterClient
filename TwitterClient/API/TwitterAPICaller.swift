533deadf12e46bff52db9e9f6304c4bcee3cb51a
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-04-21 15:21:45 -0700

    Build initial TwitterAPICaller; login and receive token

---
+++ TwitterClient/API/TwitterAPICaller.swift
@@ -0,0 +1,41 @@
+//
+//  TwitterAPICaller.swift
+//  TwitterClient
+//
+//  Created by CodePath (Dan Ndombe and Tim Lee)
+//  Modified by Maribel Montejano on 4/1/19.
+//  Copyright © 2019 Maribel Montejano. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class TwitterAPICaller: BDBOAuth1SessionManager {
+    // singleton
+    // Initialize a new BDBOAuth1SessionManager instance with the given baseURL, consumer key, and consumer secret.
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: consumerKey, consumerSecret: consumerSecret)
+    var loginSuccess: (() -> ())?
+    var loginFailure: ((Error) -> ())?
+    
+    func login(url: String, success: @escaping () -> (), failure: @escaping (Error) -> ()){
+        loginSuccess = success
+        loginFailure = failure
+        
+        // Remove the access token from the keychain before initiating the process again (in case the token is cached)
+        TwitterAPICaller.client?.deauthorize()
+        
+        // 1) Get the request token
+        // Request token gives permission to send the user to the authentication page
+        TwitterAPICaller.client?.fetchRequestToken(withPath: url, method: "GET", callbackURL: URL(string: "alamoTwitter://oauth"), scope: nil, success: { (requestToken: BDBOAuth1Credential!) in
+            // authentication URL, with request OAuth token added (authorization to send user to URL)
+            print("Got the request token")
+            
+            // Build the authentication URL, send in oauth request token
+            let url = URL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token!)")!
+            // open the URL (mobile Safari page)
+            UIApplication.shared.open(url)
+        }, failure: { (error: Error!) in
+            print("Error: \(error.localizedDescription)")
+        })
+    }
+}
--- TwitterClient/LoginViewController.swift
+++ TwitterClient/LoginViewController.swift
@@ -15,6 +15,13 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func onLoginButton(_ sender: UIButton) {
+        let myURL = "https://api.twitter.com/oauth/request_token"
         
+        // singleton Twitter Client
+        TwitterAPICaller.client?.login(url: myURL, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Could not log in!")
+        })
     }
 }
